c1f02dbb5685243842795b02516e46227b4e0f78
Justin-SG <schoenberg.justin@gmail.com>
2023-07-20 17:01:58 +0200

    Added node deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import svelte from "@astrojs/svelte";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],
-  server: { port: 5172 },
+  server: {
+    port: 3000
+  },
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
